d423a7628927ec32764b15ddc3c52ec2dfb3ac1d
Eric Boucher <eric.p.boucher@gmail.com>
2021-11-07 21:42:03 +0100

    Initialize app with react - typescript - firebase (#1)

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Hello e-read. Let's make it easier to get your news on the go.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
